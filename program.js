9a2d9192764eaa6b213d514330cfc4396b282e7d
israelevi <47865138+israelevi@users.noreply.github.com>
2019-02-21 21:22:47 +0200

    Create program.js

---
+++ program.js
@@ -0,0 +1,46 @@
+//var x=prompt("please enter nubmber",0 )
+//var y=prompt("please enter nubmber",0 )
+//if (x>y)
+  {
+alert (x)    
+  }
+//else
+{
+alert (y)  
+}
+//program2
+//var a=prompt("please enter nubmber",0 )
+//alert (a)
+//program3
+var a=prompt("please enter nubmber",0 )
+var b=prompt("please enter nubmber",0 )
+if (b==0)
+  {alert("cant")}
+else
+  {alert(a/b)}
+//program4
+var c=prompt("please enter nubmber",0 )
+if (c<10)
+ {
+  alert("1")}
+else
+if (c<100)
+ {
+  alert("2")}  
+else
+if (c<1000)
+ {
+  alert("3")
+ } 
+else
+alert ("can't")
+  
+
+
+
+
+    
+  
+
+  
+
