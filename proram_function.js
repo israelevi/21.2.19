81aff4c660d7bfa9f97bd7efef909c9af6d6ac52
israelevi <47865138+israelevi@users.noreply.github.com>
2019-02-25 19:29:26 +0200

    Create proram_function.js

---
+++ proram_function.js
@@ -0,0 +1,35 @@
+//function printaisbigger()
+{
+  console.log ("a bigger")
+}
+function printbisbigger()
+{
+  console.log ("b bigger")
+}
+//var a = parseInt(prompt("Enter number", 0))
+//var b = parseInt(prompt("Enter number", 0))
+if (a>b)
+printaisbigger()
+if (a<b)
+  printbisbigger()
+
+function average(a, b, c)
+{
+	var avg = (a+b+c)/3
+	alert (avg)
+}
+//  average(5, 10, 15)
+//
+function flip(x)
+{
+var F=x*-1
+alert (F)
+}
+var x = parseInt(prompt("Enter number", 0))
+
+flip(x)
+
+
+
+  
+
